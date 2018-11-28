a413d75b3f128e1f64d89633847cf0c8519ff78a
tmg2017 <519208032@qq.com>
2018-11-28 15:36:11 +0800

    init

---
+++ config/config.default.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * egg-sequelize-typescript default config
+ * @member Config#sequelizeTypescript
+ * @property {String} SOME_KEY - some description
+ */
+exports.sequelizeTypescript = {
+  dialect: 'mysql',
+  database: '',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '',
+};
